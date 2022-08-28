673bb3d0aa911d09dd8f80e5ea33e22f9506b7ed
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-08-28 14:52:42 -0300

    Route parameters in Express

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.get('/hello', (req, res) => res.send('Hello!'));
+app.get('/hello/:name', (req, res) => res.send(`Hello ${req.params.name}`));
 app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}!`));
 
 app.listen(8000, () => console.log('Listening on port 8000'));
