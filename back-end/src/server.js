59865f1825467a8ba5ec67a65b0a7fc41838b5f4
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-08-28 17:31:56 -0300

    Rewriting the upvote endpoint

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -24,12 +24,28 @@ app.get('/api/articles/:name', async (req, res) => {
     }
 });
 
-app.post('/api/articles/:name/upvote', (req, res) => {
-    const articleName = req.params.name;
+app.post('/api/articles/:name/upvote', async (req, res) => {
+    try {
+        const articleName = req.params.name;
 
-    articlesInfo[articleName].upvotes += 1;
+        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
 
-    res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes.`)
+        const db = client.db('blog');
+    
+        const articleInfo = await db.collection('articles').findOne({ name: articleName });
+        
+        await db.collection('articles').updateOne({ name: articleName }, {
+            '$set': { upvotes: articleInfo.upvotes + 1 }
+        });
+        
+        const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName });
+    
+        res.status(200).json(updatedArticleInfo);
+    
+        client.close();
+    } catch (error) {
+        res.status(500).json({ message: 'Error connecting to db', error });
+    }
 });
 
 app.post('/api/articles/:name/add-comment', (req, res) => {
