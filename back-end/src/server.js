ad71b8029c628ca606c9dd11333204ddb11fbf46
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-08-28 14:58:08 -0300

    Upvoting articles

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,12 +1,31 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+const articlesInfo = {
+    'learn-react': {
+        upvotes: 0,
+        comments: [],
+    },
+    'learn-node': {
+        upvotes: 0,
+        comments: [],
+    },
+    'my-thoughts-on-resumes': {
+        upvotes: 0,
+        comments: [],
+    },
+}
+
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/hello', (req, res) => res.send('Hello!'));
-app.get('/hello/:name', (req, res) => res.send(`Hello ${req.params.name}`));
-app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}!`));
+app.post('/api/articles/:name/upvote', (req, res) => {
+    const articleName = req.params.name;
+
+    articlesInfo[articleName].upvotes += 1;
+
+    res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes.`)
+});
 
 app.listen(8000, () => console.log('Listening on port 8000'));
