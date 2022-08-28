40e68a1cb2629ecece23ef2752823176e017c904
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-08-28 13:43:03 -0300

    Creating the app component

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 ```
 npx create-react-app my-blog --use-npm
 ```
+
+```
+npm install --save react-router-dom
+```
+
+---
--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
+import HomePage from './pages/HomePage';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Route path="/" component={HomePage} exact />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
