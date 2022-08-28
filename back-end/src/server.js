0ae6a0b8c1226c4b9c0ba75cc2e12b44c4413dfb
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-08-28 15:06:37 -0300

    Adding comments

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -30,6 +30,11 @@ app.post('/api/articles/:name/upvote', (req, res) => {
 
 app.post('/api/articles/:name/add-comment', (req, res) => {
     const { username, text } = req.body;
+    const articleName = req.params.name;
+
+    articlesInfo[articleName].comments.push({ username, text });
+
+    res.status(200).send(articlesInfo[articleName]);
 });
 
 app.listen(8000, () => console.log('Listening on port 8000'));
