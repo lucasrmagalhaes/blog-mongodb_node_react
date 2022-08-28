c6c15247e0ef5d1245d3831325901d816ffed78f
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-08-28 18:32:01 -0300

    Calling useEffect at the right time

--- front-end/src/pages/ArticlePage.js
+++ front-end/src/pages/ArticlePage.js
@@ -9,8 +9,8 @@ const ArticlePage = ({ match }) => {
     const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
 
     useEffect(() => {
-        setArticleInfo({ upvotes: 3 });
-    });
+        setArticleInfo({ upvotes: Math.ceil(Math.random() * 10) });
+    }, [name]);
 
     if (!article) {
         return (
